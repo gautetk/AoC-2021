ab0cdd256cd8ff41e47c4443497190f5af7dab12
Gaute Kolstad <Gaute.Kolstad@dnvgl.com>
2021-12-02 20:28:43 +0100

    refactor day 1

--- src/day1.py
+++ src/day1.py
@@ -6,18 +6,16 @@ def main(source):
     
     data = [int(l) for l in lines]
 
-    r1 = sum([checkIncrease(j - i) for i, j in zip(data[:-1], data[1:])])
+    r1 = part1(data)
     print(r1)
 
     data2 = [i + j + k for i, j, k in zip(data[:-2], data[1:-1], data[2:])]
-    r2 = sum([checkIncrease(j - i) for i, j in zip(data2[:-1], data2[1:])])
+    r2 = part1(data2)
     print(r2)
 
 
-def checkIncrease(change):
-    if change > 0:
-        return 1
-    return 0
+def part1(data):
+    return len([True for i, j in zip(data[:-1], data[1:]) if i < j])
 
 
 if __name__ == '__main__':
