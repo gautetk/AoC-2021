f23ed9ef1568f3b091d6e8dda223b00fb21c78d6
Gaute Kolstad <Gaute.Kolstad@dnvgl.com>
2021-12-04 16:20:23 +0100

    day3

--- src/day3.py
+++ src/day3.py
@@ -5,33 +5,56 @@ def main(source):
     with open(source) as r:
         lines = r.readlines()
 
-    lines = [
-        '00100',
-        '11110',
-        '10110',
-        '10111',
-        '10101',
-        '01111',
-        '00111',
-        '11100',
-        '10000',
-        '11001',
-        '00010',
-        '01010',
-    ]
+    # lines = [
+    #     '00100',
+    #     '11110',
+    #     '10110',
+    #     '10111',
+    #     '10101',
+    #     '01111',
+    #     '00111',
+    #     '11100',
+    #     '10000',
+    #     '11001',
+    #     '00010',
+    #     '01010',
+    # ]
 
-    gammaRate = ''.join([mostCommon(l) for l in zip(*lines)])
-    epsilonRate = ''.join([str(abs(int(l)-1)) for l in gammaRate])
+    gr = ''.join([mostCommon(l) for l in zip(*lines)])
+    er = ''.join([str(abs(int(l)-1)) for l in gr])
 
-    r1 = int(gammaRate, base=2) * int(epsilonRate, base=2)
+    r1 = int(gr, base=2) * int(er, base=2)
     print(r1)
 
-    
-    # print(r2)
+    ogr = oxygenRating(lines)
+    csr = co2Scrubber(lines)
+    r2 = int(ogr, base=2) * int(csr, base=2)
+    print(r2)
+
+
+def oxygenRating(data):
+    for i in range(len(data[0])):
+        col = [list(c) for c in zip(*data)][i]
+        mc = mostCommon(col)
+        data = [r for r in data if r[i] == mc]
+        if len(data) == 1:
+            return data[0]
+
+
+def co2Scrubber(data):
+    for i in range(len(data[0])):
+        col = [list(c) for c in zip(*data)][i]
+        mc = mostCommon(col)
+        lc = str(abs(int(mc) - 1))
+        data = [r for r in data if r[i] == lc]
+        if len(data) == 1:
+            return data[0]
 
 
 def mostCommon(l):
-    return max(set(l), key=l.count)
+    if l.count('1') >= l.count('0'):
+        return '1'
+    return '0'
 
 
 if __name__ == '__main__':
