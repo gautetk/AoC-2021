bf65c9141852871d294135bc8f2f57957832ff9c
Gaute Kolstad <Gaute.Kolstad@dnvgl.com>
2021-12-14 22:07:30 +0100

    day14 fix

--- src/day14.py
+++ src/day14.py
@@ -5,44 +5,54 @@ from collections import Counter
 import copy
 
 
-def main(source):
-    with open(source) as r:
+def main():
+    with open('resources\day14.txt') as r:
         input = r.read()
 
     # input = 'NNCB\n\nCH -> B\nHH -> N\nCB -> H\nNH -> C\nHB -> C\nHC -> B\nHN -> C\nNN -> C\nBH -> H\nNC -> B\nNB -> B\nBN -> B\nBB -> N\nBC -> B\nCC -> N\nCN -> C'
 
+    rules, pairs, lastChar = parse(input)
+
+    r1_pairs = runSteps(pairs, rules, 10)
+    r1 = maxMinChar(r1_pairs, lastChar)
+    print(r1)
+
+    r2_pairs = runSteps(pairs, rules, 40)
+    r2 = maxMinChar(r2_pairs, lastChar)
+    print(r2)
+
+
+def maxMinChar(pairs, lastChar):
+    chars = reduce(lambda d, kv: addTo(d, kv[0][0], kv[1]), pairs.items(), {})
+    chars[lastChar] += 1
+    return max(chars.values()) - min(chars.values())
+
+
+def addTo(d, k, v):
+    d[k] = d.get(k, 0) + v
+    return d
+
+
+def runSteps(pairs, rules, n):
+    for _ in range(n):
+        newPairs = {}
+        for k, v in pairs.items():
+            r1, r2 = rules[k]
+            newPairs[r1] = newPairs.get(r1, 0) + v
+            newPairs[r2] = newPairs.get(r2, 0) + v
+        pairs = newPairs
+
+    return pairs
+
+
+def parse(input):
     template, rules = [group.split('\n') for group in input.split('\n\n')]
     template = template[0]
 
     rules = {rule.split(' -> ')[0]: parseRule(rule) for rule in rules}
 
-    pairs = {pair: template.count(pair) for pair in rules.keys()}
-
-    r1_pairs = runSteps(pairs, rules, 10)
-    r1 = maxMinChar(r1_pairs, set(''.join(rules.keys())), template[-1])
-    print(r1)
-
-    r2_pairs = runSteps(pairs, rules, 40)
-    r2 = maxMinChar(r2_pairs, set(''.join(rules.keys())), template[-1])
-    print(r2)
-
-
-def maxMinChar(pairs, possibleChar, lastChar):
-    chars = {s: reduce(lambda i, kv: i + kv[0][0].count(s) * kv[1], pairs.items(), 0) for s in possibleChar}
-    chars[lastChar] += 1
-    return max(chars.values()) - min(chars.values())
-
-
-def runSteps(pairs, rules, n):
-    for _ in range(n):
-        tmpPairs = copy.deepcopy(pairs)
-        for k, v in tmpPairs.items():
-            r1, r2 = rules[k]
-            pairs[k] -= v
-            pairs[r1] += v
-            pairs[r2] += v
-
-    return pairs
+    pairs = {''.join(pair): template.count(''.join(pair)) for pair in zip(template[:-1], template[1:])}
+    return rules, pairs, template[-1]
 
 
 def parseRule(rule):
@@ -51,6 +61,4 @@ def parseRule(rule):
 
 
 if __name__ == '__main__':
-    challenge = 14
-    source = fr'resources\day{challenge}.txt'
-    main(source)
+    main()
