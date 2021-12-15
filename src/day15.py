c2dee274e1a3f789eeb3abe0a55ffbf53ef70b54
Gaute Kolstad <Gaute.Kolstad@dnvgl.com>
2021-12-15 22:07:57 +0100

    day15

--- src/day15.py
+++ src/day15.py
@@ -12,23 +12,24 @@ def main():
     # input = '1163751742\n1381373672\n2136511328\n3694931569\n7463417111\n1319128137\n1359912421\n3125421639\n1293138521\n2311944581'
     data = [[int(v) for v in line] for line in input.split('\n')]
 
-
     r1 = dijkstra(data)
     print(r1)
 
-    data2 = [extendListTimes(l, 5) for l in data]
-    data3 = [extend(l, i) for i in range(5) for l in data2]
+    data = [extendListTimes(l, 5) for l in data]
+    data = [extend(l, i) for i in range(5) for l in data]
 
-    r2 = dijkstra(data3)
+    r2 = dijkstra(data)
     print(r2)
 
+
 def extend(l, i):
-    return [(v + i - 1)%9 + 1 for v in l]
+    return [(v + i - 1) % 9 + 1 for v in l]
+
 
 def extendListTimes(l, n):
     newL = []
     for i in range(0, n):
-        newL += [(v + i - 1)%9 + 1 for v in l]
+        newL += [(v + i - 1) % 9 + 1 for v in l]
 
     return newL
 
@@ -37,32 +38,30 @@ def dijkstra(data):
 
     maxX = len(data[0])
     maxY = len(data)
+    end = (maxX - 1, maxY - 1)
     chitons = {(i, j): data[j][i] for j in range(maxY) for i in range(maxX)}
 
-    unvisited = set([(i,j) for j in range(maxY) for i in range(maxX)])
+    visited = set()
     lengths = {(0, 0): 0}
 
-    for _ in range(1000000):
-        current = min(lengths, key=lambda i: checkCurrent(i, lengths[i], unvisited))
-        if current == (maxX - 1, maxY - 1):
+    for _ in range(10000000):
+        current = min(lengths, key=lambda i: lengths[i])
+        currentLength = lengths[current]
+        if current == end:
             return lengths[current]
-        unvisited.remove(current)
 
-        neighboors = findNeighboors(current, unvisited)
-        for n in neighboors:
-            d = lengths[current] + chitons[n]
-            if lengths.get(n, 9999999999) > d:
-                lengths[n] = d
+        visited.add(current)
+        del lengths[current]
 
-
-
-def checkCurrent(i, n, unvisited):
-    if i in unvisited:
-        return n
-    return 99999999999
+        for n in neighbors(current, visited):
+            c = chitons.get(n)
+            if c:
+                d = currentLength + c
+                if lengths.get(n, 9999999999) > d:
+                    lengths[n] = d
 
 
-def findNeighboors(n, unvisited):
+def neighbors(n, visited):
     x, y = n
     posLoc = [
         (x + 1, y),
@@ -70,9 +69,8 @@ def findNeighboors(n, unvisited):
         (x, y + 1),
         (x, y - 1),
     ]
-    
-    return [loc for loc in posLoc if loc in unvisited]
 
+    return [loc for loc in posLoc if loc not in visited]
 
 
 if __name__ == '__main__':
